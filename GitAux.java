97a5cd48e1f471539df612db5acc626ee2fedb03
josek <josekgj@gmail.com>
2015-03-10 19:55:40 +0100

    Cambios en los tres metodos

--- GitAux.java
+++ GitAux.java
@@ -12,13 +12,14 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+        System.out.println("Linea añadida");
     }
     
     public void metodoAlumno2(){
-        System.out.println("metodo 2");
+        
     }
     
     public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
+        System.out.println("Aqui escribimos todos y yasta");
     }
 }
