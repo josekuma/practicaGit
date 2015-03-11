075a223f03db9bb6049a0abd84b861380da134db
josek <josekgj@gmail.com>
2015-03-11 22:29:19 +0100

    modificaciones por alumno2

--- GitAux.java
+++ GitAux.java
@@ -20,7 +20,7 @@ public class GitAux {
     }
     
     public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
+        
         System.out.println("josekuma");
     }
 public void testGitAux(){
